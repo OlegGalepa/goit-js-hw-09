674af2a2ee5773b146dcff7a1b32258916128c97
OlegGalepa <113933558+OlegGalepa@users.noreply.github.com>
2023-03-26 22:59:58 +0300

    task_1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const body = document.querySelector('body');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+let timerId = null;
+
+startBtn.addEventListener('click', onClickStartChangeColor);
+stopBtn.addEventListener('click', onClickStopChangeColor);
+
+function onClickStartChangeColor() {    
+  startBtn.setAttribute('disabled', '');
+
+    timerId = setInterval(() => {
+     body.style.backgroundColor = getRandomHexColor()}, 1000);
+}
+
+function onClickStopChangeColor() {
+  startBtn.removeAttribute('disabled');
+
+  clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
